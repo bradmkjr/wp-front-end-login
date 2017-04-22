935463ab3a55188695c3ba746ebaa34fc2700676
Bradford Knowlton <brad@ucanbonline.net>
2017-04-22 17:41:07 -0600

    initial commit of plugin file

---
+++ wp-front-end-login.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+Plugin Name: WP Front End Login
+Plugin URI: https://github.com/bradmkjr/WP-Front-End-Login/
+Description: Does some stuff to make front end login work.
+Version: 1.0.1
+Author: Bradford Knowlton
+Author URI: http://bradknowlton.com
+Text Domain: wpfel
+Domain Path: /languages
+License:     GNU General Public License v2.0
+License URI: https://github.com/bradmkjr/WP-Front-End-Login/blob/master/LICENSE
+*/
+
+
+
