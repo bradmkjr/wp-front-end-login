7297d77a3dace646a8e675ab23180e451cafb58e
Bradford Knowlton <brad@ucanbonline.net>
2017-04-22 18:01:19 -0600

    Added class construct

--- wp-front-end-login.php
+++ wp-front-end-login.php
@@ -13,5 +13,42 @@ License:     GNU General Public License v2.0
 License URI: https://github.com/bradmkjr/WP-Front-End-Login/blob/master/LICENSE
 */
 
+if( ! class_exists('WPFrontEndLogin') ){
 
-
+	class WPFrontEndLogin {
+	    public function __construct()
+	    {
+	    
+	    	add_shortcode( 'login_form', array( this, 'loginForm' ) );
+	    
+	    }
+	    
+	    public function loginForm(){
+		    
+		    $args = array(
+				'echo'           => false,
+				'remember'       => true,
+				'redirect'       => ( is_ssl() ? 'https://' : 'http://' ) . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'],
+				'form_id'        => 'loginform',
+				'id_username'    => 'user_login',
+				'id_password'    => 'user_pass',
+				'id_remember'    => 'rememberme',
+				'id_submit'      => 'wp-submit',
+				'label_username' => __( 'Username' ),
+				'label_password' => __( 'Password' ),
+				'label_remember' => __( 'Remember Me' ),
+				'label_log_in'   => __( 'Log In' ),
+				'value_username' => '',
+				'value_remember' => true
+			);
+		    
+		    $output = wp_login_form( $args);
+		    
+		    return $output;
+	    }
+	    
+	}
+	 
+	$wpFrontEndLogin = new WPFrontEndLogin();
+	
+}
